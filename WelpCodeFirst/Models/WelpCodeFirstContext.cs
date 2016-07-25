8454ed8959d7077f832b524ab54bb75547f3ab07
Holly Rhodes <hllyrhodes@gmail.com>
2016-07-25 16:11:09 -0400

    Controllers created

---
+++ WelpCodeFirst/Models/WelpCodeFirstContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace WelpCodeFirst.Models
+{
+    public class WelpCodeFirstContext : DbContext    
+    {
+        public DbSet<Business> Businesses { get; set; }
+        public DbSet<BusinessType> BusinessTypes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+
+        public System.Data.Entity.DbSet<WelpCodeFirst.Models.Rating> Ratings { get; set; }
+    }
+}
