7ffde651db91a874aefb2d0dfe913419671a5631
Holly Rhodes <hllyrhodes@gmail.com>
2016-07-25 16:50:08 -0400

    schema and data

--- WelpCodeFirst/Models/Business.cs
+++ WelpCodeFirst/Models/Business.cs
@@ -10,16 +10,15 @@ namespace WelpCodeFirst.Models
     {
         public int BusinessID { get; set; }
         [MaxLength(50)]
-        public string BusinessName { get; set; }
-        public int BusinessTypeID { get; set; }
+        public string BusinessName { get; set; }        
         [MaxLength(100)]
         public string Address { get; set; }
         [MaxLength(100)]
-        private string Hours { get; set; }
+        public string Hours { get; set; }
         [MaxLength(50)]
         public string Phone { get; set; }
-        private string Menu { get; set; }
-        public int UserID { get; set; }
+        public string Menu { get; set; }
+       
 
         //Navigation properties
         //used when * is pointing toward it
